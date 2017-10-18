5417311a51b279757cb0b4d0ea2e203c07860916
jackpinao <jack.pime@gmail.com>
2017-10-18 00:20:47 -0500

    1.0.0
    Primera version

---
+++ GeneradorQR/GeneradorQR/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace GeneradorQR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtValve = new System.Windows.Forms.TextBox();
+            this.btnPreview = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ResultPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // txtValve
+            // 
+            this.txtValve.Location = new System.Drawing.Point(44, 276);
+            this.txtValve.Name = "txtValve";
+            this.txtValve.Size = new System.Drawing.Size(200, 20);
+            this.txtValve.TabIndex = 1;
+            // 
+            // btnPreview
+            // 
+            this.btnPreview.Location = new System.Drawing.Point(44, 312);
+            this.btnPreview.Name = "btnPreview";
+            this.btnPreview.Size = new System.Drawing.Size(200, 23);
+            this.btnPreview.TabIndex = 2;
+            this.btnPreview.Text = "Generar";
+            this.btnPreview.UseVisualStyleBackColor = true;
+            this.btnPreview.Click += new System.EventHandler(this.btnPreview_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Enabled = false;
+            this.btnSave.Location = new System.Drawing.Point(44, 359);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(200, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Guardar";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 248);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Texto";
+            // 
+            // ResultPanel
+            // 
+            this.ResultPanel.Location = new System.Drawing.Point(44, 25);
+            this.ResultPanel.Name = "ResultPanel";
+            this.ResultPanel.Size = new System.Drawing.Size(200, 200);
+            this.ResultPanel.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 422);
+            this.Controls.Add(this.ResultPanel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnPreview);
+            this.Controls.Add(this.txtValve);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtValve;
+        private System.Windows.Forms.Button btnPreview;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel ResultPanel;
+    }
+}
+
---
+++ GeneradorQR/GeneradorQR/Form1.cs
@@ -0,0 +1,59 @@
+﻿using Gma.QrCodeNet.Encoding;
+using Gma.QrCodeNet.Encoding.Windows.Render;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace GeneradorQR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPreview_Click(object sender, EventArgs e)
+        {
+            QrEncoder qrEncoder = new QrEncoder(ErrorCorrectionLevel.H);
+            QrCode qrCode = new QrCode();
+            qrEncoder.TryEncode(txtValve.Text, out qrCode);
+
+            GraphicsRenderer renderer = new GraphicsRenderer(new FixedCodeSize(400,QuietZoneModules.Zero),Brushes.Black,Brushes.White);
+
+            MemoryStream ms = new MemoryStream();
+
+            renderer.WriteToStream(qrCode.Matrix, ImageFormat.Png, ms);
+            var imageTemporal = new Bitmap(ms);
+            var imagen = new Bitmap(imageTemporal, new Size(new Point(200, 200)));
+            ResultPanel.BackgroundImage = imagen;
+
+            //Guardar imagen en disco duro
+            /*
+            imagen.Save("imagen.png", ImageFormat.Png);
+            */
+            btnSave.Enabled = true;
+            
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Image imgFinal = (Image)ResultPanel.BackgroundImage.Clone();
+            SaveFileDialog CajaDedialogoGuardar = new SaveFileDialog();
+            CajaDedialogoGuardar.AddExtension = true;
+            CajaDedialogoGuardar.Filter = "Image PNG (*.png)|*.png";
+            CajaDedialogoGuardar.ShowDialog();
+            if (!string.IsNullOrEmpty(CajaDedialogoGuardar.FileName))
+            {
+                imgFinal.Save(CajaDedialogoGuardar.FileName, ImageFormat.Png);
+            }
+            imgFinal.Dispose();
+        }
+    }
+}
